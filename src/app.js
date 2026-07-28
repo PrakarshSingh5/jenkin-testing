7073296de1c3dbe2ba2f21d106dba288511c7e00
Prakarshsingh5 <prakarshsinghlmp@gmail.com>
2026-07-28 23:23:58 +0530

    fix: update greeting message in app.js for clarity

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from Jenkins CI/CD! I am here",
+    message: "Hello from Jenkins CI/CD! I am here just testing new things",
   });
 });
 
