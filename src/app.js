b23a28f2726a358558099f70919a5609524bc95e
Prakarshsingh5 <prakarshsinghlmp@gmail.com>
2026-07-30 00:16:19 +0530

    fix: update greeting message in app.js to include name

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello from here CI/CD! I am here just testing new things",
+    message: "Hello from here CI/CD! I am here just testing new things Prakarsh",
   });
 });
 
